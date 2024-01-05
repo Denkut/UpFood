7be00d2435ff7ce28266067fd29933799b5fa3a0
Denkutm <denkutm@gmail.com>
2024-01-06 01:36:19 +0300

    Первичная реализация BFF. Авторизация. Регистрация. Сеесся

---
+++ UpFood/src/bff/index.js
@@ -0,0 +1,4 @@
+export * from './bff';
+export * from './api/add-user';
+export * from './api/get-user';
+export * from './api/get-users';
---
+++ UpFood/src/bff/session/remove-meal.js
@@ -0,0 +1,3 @@
+export const removeMeal = () => {
+	console.log('Удаление блюда');
+};
